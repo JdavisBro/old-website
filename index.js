ec98d8cfcf2799eaae2ac3aa87e6d46163066845
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2019-10-01 21:50:24 +0100

    updated - dark theme cookies :+1: and other stuf lik emenu

---
+++ index.js
@@ -0,0 +1,64 @@
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";}
+
+function setCookie(cname, cvalue) {;
+        document.cookie = cname + "=" + cvalue + ";path=/";
+    }
+
+function getStyle(){
+        theme = getCookie('theme')
+        if (theme==="1") {document.getElementById('pagestyle').setAttribute('href','/dstyle.css');document.getElementById('toggleStyle').innerHTML = 'Light'} else {document.getElementById('pagestyle').setAttribute('href','/style.css');document.getElementById('toggleStyle').innerHTML = 'Dark'}
+    }
+
+function swapStyleSheet(){
+    x = document.getElementById('pagestyle')
+    if (x.getAttribute("href") === '/style.css') {
+        setCookie('theme','1')
+        document.getElementById('pagestyle').setAttribute('href','/dstyle.css');
+        document.getElementById('toggleStyle').innerHTML = 'Light';
+    } else {
+        setCookie('theme','0')
+        document.getElementById('pagestyle').setAttribute('href','/style.css');
+        document.getElementById('toggleStyle').innerHTML = 'Dark';      
+        }
+   }
+
+function setJdavis(){
+    document.getElementById('jdavis').value = getCookie('jdavis')
+}
+
+function changeJdavis(){
+    setCookie('jdavis',document.getElementById('jdavis').value)
+} 
+
+function setJdavisBox(){
+    if (getCookie('jdavisBox')==="1") {
+        document.getElementById('jdavisBox').checked = true;
+    }
+    else {
+        document.getElementById('jdavisBox').checked = false;
+    }
+}
+
+function changeJdavisBox(){
+    if (getCookie('jdavisBox')==='1') {
+        setCookie('jdavisBox','0')
+    }
+    else {
+        setCookie('jdavisBox','1')
+    }
+}
+
+getStyle()
